296f8a0c5ade7a3bb247e3959228134ffc2eafc3
Hiroyuki Hasebe <16788901+levi106@users.noreply.github.com>
2022-12-03 19:45:08 +0900

    debug print

--- utils/buildContainerTemplate.sh
+++ utils/buildContainerTemplate.sh
@@ -24,7 +24,9 @@ set -euxo pipefail  # fail on error
 # Generate an tag with a reproducible checksum of all files in . by doing a checksum of all files
 # in alphabetical order, then another checksum of their names and checksums.
 # Running this command on windows-based infrastructure may return a different result due to CRLF
+pwd
 pushd $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
+pwd
 imageTag=$(git log -n 1 --format="%H" -- ".")
 popd
   
